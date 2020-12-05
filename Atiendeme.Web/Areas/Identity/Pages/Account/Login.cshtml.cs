59d809caf735e5c669ea1a046a2d5ff20afc750d
Leidy Gonzalez <leidygonzalez0210@gmail.com>
2020-12-05 01:51:41 -0400

    home

--- Atiendeme.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Atiendeme.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -79,7 +79,10 @@ namespace Atiendeme.Web.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+
+                //change this for identity -CheckPasswordSignInAsync
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
--- Atiendeme.Web/wwwroot/js/site.js
+++ Atiendeme.Web/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿(function ($) {
+    const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = () => {
+        navbar.classList.add("show");
+        menuBtn.classList.add("hide");
+        body.classList.add("disabled");
+    }
+    cancelBtn.onclick = () => {
+        body.classList.remove("disabled");
+        navbar.classList.remove("show");
+        menuBtn.classList.remove("hide");
+    }
+    window.onscroll = () => {
+        this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+});
