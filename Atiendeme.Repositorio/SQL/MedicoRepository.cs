43ab9e702a24764e1224cb38dd443a622148318c
Leidy Gonzalez <leidygonzalez0210@gmail.com>
2020-12-05 22:55:14 -0400

    "better" query

--- Atiendeme.Repositorio/SQL/MedicoRepository.cs
+++ Atiendeme.Repositorio/SQL/MedicoRepository.cs
@@ -19,14 +19,29 @@ namespace Atiendeme.Repositorio.SQL
 
         public async Task<List<ApplicationUser>> ObtenerMedicosAsync()
         {
-            //This can get better with a better model
+            //This can get fix with a better model
 
-            var role = await _applicationDbContext.AspNetRoles.FirstOrDefaultAsync(j => j.Name == "Doctor");
-            var usersInRole = await _applicationDbContext.AspNetUserRoles.Where(j => j.RoleId == role.Id)
-                                    .Select(x => x.UserId)
-                                    .ToListAsync();
+            //var role = await _applicationDbContext.AspNetRoles.FirstOrDefaultAsync(j => j.Name == "Doctor");
+            //var usersInRole = await _applicationDbContext.AspNetUserRoles.Where(j => j.RoleId == role.Id)
+            //                        .Select(x => x.UserId)
+            //                        .ToListAsync();
+            //var users = await _applicationDbContext.AspNetUsers.Where(user => usersInRole.Contains(user.Id)).ToListAsync();
 
-            var users = await _applicationDbContext.AspNetUsers.Where(user => usersInRole.Contains(user.Id)).ToListAsync();
+            var users = await (from u in _applicationDbContext.AspNetUsers
+                               join ur in _applicationDbContext.AspNetUserRoles on u.Id equals ur.UserId
+                               join r in _applicationDbContext.AspNetRoles on ur.RoleId equals r.Id
+                               where (r.Name == "Doctor")
+                               select new ApplicationUser
+                               {
+                                   Email = u.Email,
+                                   PhoneNumber = u.PhoneNumber,
+                                   Id = u.Id,
+                                   Genre = u.Genre,
+                                   Name = u.Name,
+                                   LastName = u.LastName,
+                                   UserName = u.UserName,
+                                   Birthday = u.Birthday,
+                               }).ToListAsync();
 
             return users;
         }
