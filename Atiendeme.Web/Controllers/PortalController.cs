2ccc4e09d3236620b569f4425a68ce9395708a2d
Leidy Gonzalez <leidygonzalez0210@gmail.com>
2020-12-08 23:43:18 -0400

    Added SPA

---
+++ Atiendeme.Web/Controllers/PortalController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atiendeme.Web.Controllers
+{
+    [Authorize]
+    public class PortalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
