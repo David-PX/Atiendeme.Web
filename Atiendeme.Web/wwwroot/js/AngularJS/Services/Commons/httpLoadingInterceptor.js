c61de12a88cca02b673d5ba3d8fe04de90bc71b7
Leidy Gonzalez <leidygonzalez0210@gmail.com>
2020-12-10 02:18:09 -0400

    funny interceptor added

---
+++ Atiendeme.Web/wwwroot/js/AngularJS/Services/Commons/httpLoadingInterceptor.js
@@ -0,0 +1,29 @@
+﻿app.factory('httpLoadingInterceptor', function ($q, $rootScope, $log) {
+    var numLoadings = 0;
+
+    return {
+        request: function (config) {
+            if (numLoadings === 0)
+                $('#loader').css('display', 'block');
+
+            numLoadings++;
+            return config || $q.when(config)
+        },
+
+        response: function (response) {
+            if ((--numLoadings) === 0)
+                $('#loader').css('display', 'none');
+
+            return response || $q.when(response);
+        },
+        responseError: function (response) {
+            if ((--numLoadings) === 0)
+                $('#loader').css('display', 'none');
+            return $q.reject(response);
+        }
+    };
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('httpLoadingInterceptor');
+});
